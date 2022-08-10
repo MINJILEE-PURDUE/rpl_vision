5474bb85846be458869cac10c7da75de4abba1ea
MINJI LEE <56851781+MINJILEE-PURDUE@users.noreply.github.com>
2022-08-10 15:32:06 -0500

    Update detect_pubsub.py

--- detect_pubsub.py
+++ detect_pubsub.py
@@ -251,6 +251,36 @@ def main(opt):
     check_requirements(exclude=('tensorboard', 'thop'))
     run(**vars(opt))
 
+    
+# pubsub on ROS  
+import rospy
+from std_msgs.msg import String
+  
+def publisher():
+    # define the actions the publisher will make
+    pub = rospy.Publisher('/<topic_name>',
+                          String, queue_size=10)
+    # initialize the publishing node
+    rospy.init_node('<node_name>', anonymous=True)
+      
+    # define how many times per second
+    # will the data be published
+    # 10 times/second or 10Hz as an example
+    rate = rospy.Rate(10)
+    # to keep publishing as long as the core is running
+    while not rospy.is_shutdown():
+        data = "The data that you wish to publish."
+          
+        # you could simultaneously display the data
+        # on the terminal and to the log file
+        rospy.loginfo(data)
+          
+        # publish the data to the topic using publish()
+        pub.publish(data)
+          
+        # keep a buffer based on the rate defined earlier
+        rate.sleep()
+  
 
 if __name__ == "__main__":
     opt = parse_opt()
